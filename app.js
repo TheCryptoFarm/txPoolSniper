b8fb335e4069c30032209cce86387f8641f3714a
DC <drunkencrypto@protonmail.com>
2021-06-03 21:36:52 -0400

    Add command line options, sell function

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 "use strict";
 const env = require("./env.json");
+const {Command} = require("commander");
+const program = new Command();
+
 Object.assign(process.env, env);
 
 const ethers = require("ethers");
 const retry = require("async-retry");
 const tokens = require("./tokens.js");
 
+const tokenToTransact = tokens.pair[1];
 const purchaseAmount = ethers.utils.parseUnits(tokens.purchaseAmount, "ether");
 const pcsAbi = new ethers.utils.Interface(require("./abi.json"));
 const EXPECTED_PONG_BACK = 30000;
@@ -17,7 +21,7 @@ const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
 const account = wallet.connect(provider);
 const router = new ethers.Contract(tokens.router, pcsAbi, account);
 
-const startConnection = () => {
+const startConnection = (buyOrSellAction) => {
   let pingTimeout = null;
   let keepAliveInterval = null;
   provider._websocket.on("open", () => {
@@ -48,7 +52,7 @@ const startConnection = () => {
               if (
                 ethers.utils.getAddress(tokens.pair[1]) === decodedInput.args[0]
               ) {
-                await BuyToken(tx);
+                await buyOrSellAction(tx);
               }
             }
           }
@@ -65,7 +69,7 @@ const startConnection = () => {
   });
 
   provider._websocket.on("error", () => {
-    console.log("Error. Attemptiing to Reconnect...");
+    console.log("Error. Attempting to Reconnect...");
     clearInterval(keepAliveInterval);
     clearTimeout(pingTimeout);
     startConnection();
@@ -77,6 +81,48 @@ const startConnection = () => {
 };
 
 const BuyToken = async (txLP) => {
+  const tx = await retry(
+    async () => {
+      // sell all
+      const amounts = await router.getAmountsIn(0, tokens.pair);
+      const amountInMin = amounts[0].sub(amounts[0].div(tokens.slippage));
+      let sellConfirmation =
+        await router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+          amountInMin,
+          tokens.pair,
+          process.env.RECIPIENT,
+          Date.now() + 1000 * 60 * 5, //5 minutes
+          {
+            value: purchaseAmount,
+            gasLimit: txLP.gasLimit,
+            gasPrice: txLP.gasPrice,
+          }
+        );
+      return sellConfirmation;
+    },
+    {
+      retries: 5,
+      minTimeout: 10000,
+      maxTimeout: 15000,
+      onRetry: (err, number) => {
+        console.log("Buy Failed - Retrying", number);
+        console.log("Error", err);
+        if (number === 5) {
+          console.log("Sniping has failed...");
+          process.exit();
+        }
+      },
+    }
+  );
+  console.log("Waiting for Transaction receipt...");
+  const receipt = await tx.wait();
+  console.log("Token Sell Complete");
+  console.log("Associated LP Event txHash: " + txLP.hash);
+  console.log("Your txHash: " + receipt.transactionHash);
+  process.exit();
+};
+
+const SellToken = async (txLP) => {
   const tx = await retry(
     async () => {
       const amounts = await router.getAmountsOut(purchaseAmount, tokens.pair, {
@@ -119,4 +165,34 @@ const BuyToken = async (txLP) => {
   console.log("Your txHash: " + receipt.transactionHash);
   process.exit();
 };
-startConnection();
+program
+.version('0.0.1', '-v, --version')
+.command('buy [token]')
+.description('buy token by 0x address, defaults to value in tokens.js: ' + tokenToTransact) // tokenToTransact)
+.action((token) => {
+  if (token == undefined) {
+    token = tokenToTransact;
+  }
+  tokens.pair[1] = token;
+  console.log('Will attempt to buy ' + token);
+  startConnection(BuyToken);
+});
+
+program
+.command('sell [token]')
+.description('sell token by 0x address, defaults to value in tokens.js: ' + tokenToTransact) // , tokenToTransact);
+.action((token) => {
+  if (token == undefined) {
+    token = tokenToTransact;
+  }
+  console.log('Will attempt to sell ' + token);
+  tokens.pair[1] = token;
+  startConnection(SellToken);
+});
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+console.log(options);
+
