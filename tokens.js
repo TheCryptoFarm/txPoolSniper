b4cac4b9a5de530925fcf9095fb690a13ee3c5d9
PhatJay <jj@indie.org>
2021-12-31 04:41:22 -0500

    Money's only something you need in case you don't die tomorrow

--- tokens.js
+++ tokens.js
@@ -19,7 +19,6 @@ module.exports = tokens;
  BSC Mainnet:
 
   router: "0x10ED43C718714eb63d5aA57B78B54704E256024E", // PCSv2 Router Mainnet
- 
   pair: [
     "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // WBNB [from]
     "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82", // CAKE [to]
